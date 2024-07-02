00fced30d8e561e1f685664e70091f7e9e974b61
Shendri Kenneth Yamba <shendrikennethyamba@Shendris-MacBook-Pro.local>
2024-07-02 18:36:46 +0800

    try

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import React from 'react'
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>try</div>
-      <div>try</div>
-      <div>try</div>
+   <>
+    
     </>
   )
 }
